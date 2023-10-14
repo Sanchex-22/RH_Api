f0146685b5d50b143ebf457c7424dfcf5dd78529
Abiel Sanchez <abiel02sz@gmail.com>
2023-10-13 19:14:14 -0500

    cambiodeunimport

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,4 +1,4 @@
-import { UserController } from '../../controllers/userController'
+import { UserController } from '../controllers/user_Controller'
 import { Router } from 'express'
 
 const userrouter = Router()
