e2f959303f2c605f622899b73b30c396db67f93f
Sanchex-22 <David21.cs70@gmail.com>
2023-10-20 11:11:39 -0500

    Cambios en mi package.json y pruebas con el login y db config

--- src/database/dbConnect.mjs
+++ src/database/dbConnect.mjs
@@ -1,8 +1,23 @@
 import { Sequelize } from 'sequelize'
+const config = require('../../config/dbConfig.mjs')
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
-})
+const sequelize = new Sequelize(
+  config.DB,
+  config.USER,
+  config.PASSWORD,
+  {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  }
+)
+
+const db = {}
+db.Sequelize = Sequelize
+// Asigna el objeto Sequelize al objeto db
+db.sequelize = sequelize
+
+/* -- Importa el modelo de usuario y lo asocia con la conexión a la base de datos -- */
+db.user = require('../database/usersModels.mjs')(sequelize, Sequelize)
+module.exports = db
 
 export default sequelize
