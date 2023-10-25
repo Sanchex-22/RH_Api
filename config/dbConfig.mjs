29e983d61409031fff635ad0babfce6178b05a11
Sanchex-22 <David21.cs70@gmail.com>
2023-10-25 13:40:03 -0500

    configuracion del .env

--- config/dbConfig.mjs
+++ config/dbConfig.mjs
@@ -1,17 +1,16 @@
-import { config } from 'dotenv'
-config({ path: '../.env' })
+import 'dotenv/config'
 
 export const dbConfig = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '12345',
-  DB: 'recursoshumanos',
-  dialect: 'mysql',
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
-    max: parseInt('8'),
-    min: parseInt('0'),
-    acquire: parseInt('300000'),
-    idle: parseInt('100000')
+    max: parseInt(process.env.DB_POOL_MAX),
+    min: parseInt(process.env.DB_POOL_MIN),
+    acquire: parseInt(process.env.DB_POOL_ACQUIRE),
+    idle: parseInt(process.env.DB_POOL_IDLE)
   }
 }
 export default dbConfig
