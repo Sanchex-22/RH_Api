ca71875eaba49e6e34a90d2d10c36f6e41641f67
Sanchex-22 <David21.cs70@gmail.com>
2023-11-07 14:47:03 -0500

    jwt protected routes

--- src/middleware/AuthMiddleware.mjs
+++ src/middleware/AuthMiddleware.mjs
@@ -1,21 +1,29 @@
 import jwt from 'jsonwebtoken'
 export class AuthMiddlewire {
   static Authorization = (request, response, next) => {
-    const Authorization = request.get('authorization')
-    let token = ''
+    try {
+      const Authorization = request.get('authorization')
+      let token = ''
 
-    if (Authorization && Authorization.toLowerCase().startsWith('bearer')) {
-      token = Authorization.substring(7)
+      if (Authorization && Authorization.toLowerCase().startsWith('bearer')) {
+        token = Authorization.substring(7)
+      }
+      console.log(token)
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+      console.log(decodedToken)
+      if (!token || !decodedToken.userToken.id) {
+        return response.status(401).json({ error: 'token invalido o ha expirado' })
+      }
+      const { id: userId } = decodedToken.userToken
+      request.userId = userId
+      console.log(userId)
+      next()
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        response.status(401).json({ error: 'token expirado' })
+      } else {
+        response.status(401).json({ error: 'Error al verificar el token:' })
+      }
     }
-    console.log(token)
-    const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
-    console.log(decodedToken)
-    if (!token || !decodedToken.userToken.id) {
-      return response.status(401).json({ error: 'token invalido o ha expirado' })
-    }
-    const { id: userId } = decodedToken.userToken
-    request.userId = userId
-    console.log(userId)
-    next()
   }
 }
